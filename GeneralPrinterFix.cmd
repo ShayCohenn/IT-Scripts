203ea00b818e0d38be30e4336c79c50ba5f5f04b
Shay Cohen <127669502+ShayCohenn@users.noreply.github.com>
2025-11-10 11:55:40 +0200

    Update GeneralPrinterFix.cmd

--- GeneralPrinterFix.cmd
+++ GeneralPrinterFix.cmd
@@ -1,11 +1,16 @@
+:: Stop Printer spooler
 net stop spooler >nul 2>&1
 
+:: Clear printer queue
 del /f /q "%systemroot%\System32\spool\PRINTERS\*" >nul 2>&1
 
+:: Resetting cache
 reg delete "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Devices" /f >nul 2>&1
 reg delete "HKCU\Software\Microsoft\Windows NT\CurrentVersion\PrinterPorts" /f >nul 2>&1
 reg delete "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v Device /f >nul 2>&1
 
+:: Start printer spooler
 net start spooler >nul 2>&1
 
+:: Clear DNS cache
 ipconfig /flushdns >nul
