a7ad57704825d69d385f977f6d154ebc4fa37824
Shay Cohen <127669502+ShayCohenn@users.noreply.github.com>
2025-11-14 12:26:02 +0200

    Update run.cmd

--- InternetFix/run.cmd
+++ InternetFix/run.cmd
@@ -1,69 +1,22 @@
-:: Close browsers
-
+:: Close Browsers
 taskkill /f /im msedge.exe >nul 2>&1
-
 taskkill /f /im chrome.exe >nul 2>&1
 
- 
-
-:: Update policies
-
+:: Update policies 
 gpupdate /force
 
- 
-
 :: Clear DNS cache
-
 ipconfig /flushdns
-
- 
-
-:: Reset all to default
-
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones" /f
-
- 
 
 timeout /t 1 >nul
 
- 
-
-:: Local Intranet
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1" /v CurrentLevel /t REG_DWORD /d 0x0 /f
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1" /v MinLevel /t REG_DWORD /d 0x0 /f
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1" /v RecommendedLevel /t REG_DWORD /d 0x0 /f
-
- 
-
-:: Trusted Sites
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2" /v CurrentLevel /t REG_DWORD /d 0x0 /f
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2" /v MinLevel /t REG_DWORD /d 0x0 /f
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2" /v RecommendedLevel /t REG_DWORD /d 0x0 /f
-
- 
-
-:: Internet
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v CurrentLevel /t REG_DWORD /d 0x0 /f
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v MinLevel /t REG_DWORD /d 0x0 /f
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v RecommendedLevel /t REG_DWORD /d 0x0 /f
-
- 
+:: Lower zones security
+reg import Zone1.reg
+reg import Zone2.reg
+reg import Zone3.reg
 
 :: Turn off popup blocker
-
 reg add "HKCU\Software\Microsoft\Internet Explorer\New Windows" /v PopupMgr /t REG_DWORD /d 0 /f
 
- 
-
 :: Clear SSL state
-
 "%SystemRoot%\System32\rundll32.exe" "%SystemRoot%\System32\wininet.dll",DispatchAPICall 3
